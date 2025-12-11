8fa5e218531c1c48bc611059f904457d45210c49
Hải Tân <221124022310@due.udn.vn>
2025-12-11 13:19:38 +0700

    Update Sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import {
   FaChartLine,
   FaUsers,
+  FaUser,
   FaCog,
   FaRestroom,
   FaRegQuestionCircle,
@@ -59,7 +60,7 @@ const menuItems = [
   },
   {
     name: "Staff",
-    icon: <FaUsers />,
+    icon: <FaUser />,
     path: "/staff",
     rolesAllowed: ["admin"],
   },
@@ -99,7 +100,7 @@ export default function Sidebar({ isOpen, toggle }: SidebarProps) {
   return (
     <motion.div
       animate={{ width: isOpen ? 200 : 52 }}
-      className="h-screen bg-white shadow-md flex flex-col overflow-hidden relative text-black transition-all duration-200"
+      className="h-screen bg-white shadow-md flex flex-col overflow-hidden relative text-black transition-all duration-100"
     >
       <div className="flex h-[57px] items-center px-2 border-b border-gray-200">
         <button
@@ -150,6 +151,7 @@ export default function Sidebar({ isOpen, toggle }: SidebarProps) {
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       exit={{ opacity: 0 }}
+                      className="text-nowrap"
                     >
                       {item.name}
                     </motion.span>
@@ -168,7 +170,7 @@ export default function Sidebar({ isOpen, toggle }: SidebarProps) {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="text-gray-600 text-sm"
+              className="text-gray-600 text-sm text-nowrap"
             >
               &copy; 2025 Instay Application
             </motion.div>
